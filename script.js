263d72bed597cde7cd07dab204b91c697e6afddf
Burak Baloglu <burakbaloglu148@gmail.com>
2022-05-22 01:09:41 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector('#menuBtn');
+let navbar = document.querySelector('.header .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
